596e58f67eec11487b85ef282eb93b428a224dd3
baldje <kolodeznikov1997@gmail.com>
2022-04-07 00:50:12 +0900

    firstCommit

---
+++ jfr/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Header/>
+  <NewsBar/>
+  <p>kek cheburek</p>
+  <keep-alive/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import NewsBar from "@/components/Feed/NewsBar";
+import Header from "@/components/Header";
+
+export default {
+  name: 'App',
+  components: {
+    NewsBar,
+    Header,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ jfr/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-button>
+          HelloImButton
+        </v-button>
+      </v-col>
+    </v-row>
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  name: "HeaderForPractice"
+}
+</script>
+
+<style scoped>
+
+</style>
